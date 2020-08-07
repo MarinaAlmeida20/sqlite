6311f4e9c978cf90a9f51b67a6b134a43b2d2754
Marina <marinabrito.ma@gmail.com>
2020-08-07 02:05:58 +0100

    Order by

--- database.py
+++ database.py
@@ -6,22 +6,14 @@ conn = sqlite3.connect('customer.db')
 # Create a cursor
 c = conn.cursor()
 
-# Update Records
+# Query the Database - ORDER BY 
+c.execute("SELECT rowid, * FROM customers ORDER BY rowid DESC")
 
-c.execute("""UPDATE customers SET first_name = 'Bob'
-            WHERE last_name = 'Elder'
-        """)
-
-conn.commit()
-
-# Create a Table
-c.execute("SELECT rowid, * FROM customers")
-#print(c.fetchone()[0])
-#print(c.fetchmany(3))
 items = c.fetchall()
 
 for item in items:
     print(item)
+
 
 
 
