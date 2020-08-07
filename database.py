afe5a5389132ac63a06c3185b5a4afdb92d49c60
Marina <marinabrito.ma@gmail.com>
2020-08-07 01:16:35 +0100

    .

--- database.py
+++ database.py
@@ -6,8 +6,14 @@ conn = sqlite3.connect('customer.db')
 # Create a cursor
 c = conn.cursor()
 
+many_customers = [
+                    ('Wes', 'Brown', 'wes@brown.com'),
+                    ('Steph', 'Kuewa', 'steph@kuewa.com'), 
+                    ('Dan', 'Pas', 'dan@pas.com'),
+                ]
+
 # Create a Table
-c.execute("INSERT INTO customers VALUES ('John', 'Elder', 'john@codemy.com')")
+c.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
 
 
 
